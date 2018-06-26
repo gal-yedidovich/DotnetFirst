181e923c8cd0d6e89a11d61933f0b4f8a6637c08
m17guy <m1734guy@gmail.com>
2018-06-26 19:28:36 +0200

    it's a' me!

---
+++ star-pyramid/a soducode test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_soducode_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input hight");
+            int hight = Int32.Parse(Console.ReadLine());
+            for (int i=1; i <= hight; i++)
+            {
+                Console.Write("*");
+                for (int line=1; line < i; line++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
